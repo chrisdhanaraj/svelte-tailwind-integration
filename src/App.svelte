1640f5e61166fa1d5367eda38f1749a9b781dc11
Chris Dhanaraj <cdhanaraj@netflix.com>
2020-09-08 10:52:17 -0700

    raw svelte test

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let name;
+</script>
+
+<style global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p class="p-2">
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+</main>
